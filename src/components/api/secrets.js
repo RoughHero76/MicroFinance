6a4bb47bbd043cd68b6a567bfec84194ec587001
Faizan Khan <m.faisalkhan9@gmail.com>
2024-12-03 14:08:21 +0530

    Update secrets.js

--- src/components/api/secrets.js
+++ src/components/api/secrets.js
@@ -1,6 +1,3 @@
 //src/components/api/secrets.js
 
 export const API_URL = 'http://192.168.160.212:5000';
-
-//https://microfinanace-g4fba3bye5cdggee.centralindia-01.azurewebsites.net
-//http://192.168.160.212:5000
