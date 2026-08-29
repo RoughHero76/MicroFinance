ff3a9a4668dd325c30783dcca3c761d07c9e9116
RoughHero76 <m.faisalkhan9@gmail.com>
2026-08-29 20:10:39 +0530

    refactor: minor improvement

--- src/components/api/secrets.js
+++ src/components/api/secrets.js
@@ -1,6 +1,6 @@
 //src/components/api/secrets.js
 
-export const API_URL = 'http://microfinance-backend.76softwares.in';
+export const API_URL = 'https://microfinance-backend.76softwares.in';
 
 //https://microfinanace-g4fba3bye5cdggee.centralindia-01.azurewebsites.net
 //http://192.168.0.2:5000
